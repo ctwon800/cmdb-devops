7959ed59146353f221db3dd7458fd1c4bc394e52
chentong <chentong@rightknights.com>
2025-01-08 14:27:50 +0800

    优化配置

--- backend/application/asgi.py
+++ backend/application/asgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
