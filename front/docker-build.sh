4e24673bbc087d1ff68dca6cd23e4738c354ac1d
chentong <chentong@rightknights.com>
2025-01-17 14:50:06 +0800

    删除sh文件front

--- front/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-npm run build
-mv dist docker-build
-version=$1
-echo $version
-docker build -t registry-docker.rightknights.com/devops/cmdb-front:$version ./docker-build/
-docker push registry-docker.rightknights.com/devops/cmdb-front:$version
-rm -rf docker-build/dist
