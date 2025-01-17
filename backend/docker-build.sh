cb0d720d9d7dda2bc725fc76e5d05aa895544b84
chentong <chentong@rightknights.com>
2025-01-17 14:49:34 +0800

    删除sh文件

--- backend/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-version=$1
-echo $version
-docker build -t registry-docker.rightknights.com/devops/cmdb-back:$version .
-docker push registry-docker.rightknights.com/devops/cmdb-back:$version
